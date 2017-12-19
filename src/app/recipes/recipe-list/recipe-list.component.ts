ee9a73feca6b7ee67707184d3e1af9aedfc50f22
Awan <AwanH@corp.root.nasd.com>
2017-12-19 15:55:39 -0500

    Second commit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,10 @@ import { Recipe } from "../recipe.model";
   styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [new Recipe("A Test Recipe", "This is simply a test", "http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg")];
+  recipes: Recipe[] = [
+    new Recipe("A Test Recipe", "This is simply a test", "http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg"),
+    new Recipe("A Test Recipe 2", "This is simply a test 2", "http://media2.sailusfood.com/wp-content/uploads/2016/03/recipe-of-momos.jpg")
+  ];
 
   constructor() {}
 
